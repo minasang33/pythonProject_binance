d36f5215c4056bcddef7dae5b806fb69c8e1a199
hyeminwang <minasang33@gmail.com>
2023-02-24 11:30:04 +0900

    add requirements.txt

--- app.py
+++ app.py
@@ -159,24 +159,24 @@ def stop(update, context):
 def clear(update, context):
     bot.stopPoll()
 
-@app.route('/')
-def index():
-    print('main')
+# @app.route('/')
+# def index():
+print('main')
 
-    bot.send_message(chat_id=MY_ID, text='안녕하세요!! \n 작업을 시작하고플땐 /hstart \n 작업을 중지하고플땐 /stop \n 메세지를 전송해주세요.😄')
+bot.send_message(chat_id=MY_ID, text='안녕하세요!! \n 작업을 시작하고플땐 /hstart \n 작업을 중지하고플땐 /stop \n 메세지를 전송해주세요.😄')
 
-    updater = Updater(token=BOT_TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
+updater = Updater(token=BOT_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-    start_handler = CommandHandler('hstart', start)
-    stop_handler = CommandHandler('stop', stop)
+start_handler = CommandHandler('hstart', start)
+stop_handler = CommandHandler('stop', stop)
 
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(stop_handler)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(stop_handler)
 
-    updater.start_polling()
-    updater.idle()
-    return render_template('index.html')
+updater.start_polling()
+updater.idle()
+    # return render_template('index.html')
 
-if __name__ =='__main__':
-    app.run()
+# if __name__ =='__main__':
+#     app.run()
